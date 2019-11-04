7049c9441bf56b2188db3130105a282e6ef1bc92
aneesh-khatawkar <57169985+aneesh-khatawkar@users.noreply.github.com>
2019-11-04 13:21:51 +0530

    Add files via upload

---
+++ DB.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // DBname : 'mongodb://localhost:27017/DB_Product'
+
+  DBname:
+    "mongodb+srv://aneesh:aneesh123@product-api-wqsyj.mongodb.net/NewsDB?retryWrites=true&w=majority"
+};
